c7e8936937b9da6a589ece19099fadeb2fb6d3c2
manveer <m13basra@gmail.com>
2025-04-11 10:52:45 -0700

    Update test/AITokenTransferRole.t.sol

--- test/AITokenTransferRole.t.sol
+++ test/AITokenTransferRole.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "../src/AITokenTransferRole.sol"; // adjust path if needed
+import "../src/AITokenTransferRole.sol";
 
 contract AITokenTransferRoleTest is Test {
     AITokenTransferRole token;
